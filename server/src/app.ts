c9f7c1d34b45064218efa8c007723217a7b7cfb0
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-03-06 01:02:40 +0100

    Initialized server configutations and project structure

---
+++ server/src/app.ts
@@ -0,0 +1,45 @@
+import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import os from "os";
+import { StatusCodes } from "http-status-codes";
+
+const app: Application = express();
+
+// Middleware setup
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test route
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  const currentDateTime = new Date().toISOString();
+  const clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const serverHostname = os.hostname();
+  const serverPlatform = os.platform();
+  const serverUptime = os.uptime();
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    message: "Welcome to the LinkTutor",
+    version: "1.0.0",
+    clientDetails: {
+      ipAddress: clientIp,
+      accessedAt: currentDateTime,
+    },
+    serverDetails: {
+      hostname: serverHostname,
+      platform: serverPlatform,
+      uptime: `${Math.floor(serverUptime / 60 / 60)} hours ${Math.floor(
+        (serverUptime / 60) % 60
+      )} minutes`,
+    },
+    developerContact: {
+      email: "devops.tuc@gmail.com",
+      website: "https://ashiqdev.vercel.app/",
+    },
+  });
+});
+
+export default app;
