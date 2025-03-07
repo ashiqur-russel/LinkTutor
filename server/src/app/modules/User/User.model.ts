fba80501cf44bd26fde70600a17dbaeeefc7af40
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-03-07 18:36:09 +0100

    Save hasehd password and add mongoose pre save middleware to check user existence

--- server/src/app/modules/User/User.model.ts
+++ server/src/app/modules/User/User.model.ts
@@ -1,5 +1,9 @@
 import mongoose, { Schema, Model } from "mongoose";
 import { IUser, UserRole } from "./User.interface";
+import AppError from "../../errors/appError";
+import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
 const AddressSchema: Schema = new Schema({
   street: { type: String, required: true },
@@ -23,6 +27,54 @@ const UserSchema: Schema<IUser> = new Schema(
     timestamps: true,
   }
 );
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+
+  next();
+});
+
+UserSchema.post("save", function (doc, next) {
+  doc.password = "";
+  next();
+});
+
+UserSchema.set("toJSON", {
+  transform: (_doc, ret) => {
+    delete ret.password;
+    return ret;
+  },
+});
+
+UserSchema.statics.isPasswordMatched = async function (
+  plainTextPassword,
+  hashedPassword
+) {
+  return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+UserSchema.statics.isUserExistsByEmail = async function (email: string) {
+  return await User.findOne({ email }).select("+password");
+};
+
+UserSchema.statics.checkUserExist = async function (userId: string) {
+  const existingUser = await this.findById(userId);
+
+  if (!existingUser) {
+    throw new AppError(StatusCodes.NOT_ACCEPTABLE, "User does not exist!");
+  }
+
+  if (!existingUser.isActive) {
+    throw new AppError(StatusCodes.NOT_ACCEPTABLE, "User is not active!");
+  }
+
+  return existingUser;
+};
 
 const User: Model<IUser> = mongoose.model<IUser>("User", UserSchema);
 export default User;
