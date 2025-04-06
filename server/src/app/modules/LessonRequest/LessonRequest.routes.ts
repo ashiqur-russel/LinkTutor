1ecb0ec49914345cc1273027704b7447a7a46e47
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-04-06 05:05:34 +0200

    Add auth guard on router to inhance api security

--- server/src/app/modules/LessonRequest/LessonRequest.routes.ts
+++ server/src/app/modules/LessonRequest/LessonRequest.routes.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import { lessonRequestControllers } from "./lessonRequest.controller";
-import { TutorController } from "../Tutor/Tutor.controller";
-import { Auth } from "../Auth/Auth.model";
 import { UserRole } from "../User/User.interface";
 import auth from "../../middleware/auth";
 
@@ -13,25 +11,39 @@ router.post(
   lessonRequestControllers.createLessonRequest
 );
 
-router.get("/", lessonRequestControllers.getAllLessonRequests);
+router.get(
+  "/",
+  auth(UserRole.ADMIN),
+  lessonRequestControllers.getAllLessonRequests
+);
 
-router.get("/:userId/my-request", lessonRequestControllers.getMyLessonRequest);
+router.get(
+  "/:userId/my-request",
+  auth(UserRole.STUDENT, UserRole.TUTOR),
+  lessonRequestControllers.getMyLessonRequest
+);
 router.get(
   "/:userId/my-future-request",
+  auth(UserRole.STUDENT, UserRole.TUTOR),
   lessonRequestControllers.getMyUpcomingLessonRequest
 );
 
 router.post(
   "/:id/decline-request",
+  auth(UserRole.TUTOR),
   lessonRequestControllers.declineLessonRequest
 );
 
 router.post(
   "/:id/cancel-request",
-
+  auth(UserRole.STUDENT),
   lessonRequestControllers.cancelessonRequest
 );
 
-router.post("/:id/accept-request", lessonRequestControllers.acceptRequest);
+router.post(
+  "/:id/accept-request",
+  auth(UserRole.TUTOR),
+  lessonRequestControllers.acceptRequest
+);
 
 export const LssonRequestrRoutes = router;
